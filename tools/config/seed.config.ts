3dcdcf4ec5440123ca2c8cc3f80011e300949613
Ryan Wilson <Ryan@MacBook-Pro.local>
2016-08-08 09:57:34 -0700

    Changing title

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -138,7 +138,7 @@ export class SeedConfig {
    * `index.html`.
    * @type {string}
    */
-  APP_TITLE = 'Welcome to angular2-seed!';
+  APP_TITLE = 'Up for a challenge?!';
 
   /**
    * The base folder of the applications source files.
