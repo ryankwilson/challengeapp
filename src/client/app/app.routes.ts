4a5b15d52916fa2db0b54319295142f08aeca1e0
Ryan Wilson <Ryan@MacBook-Pro.local>
2016-08-11 20:51:33 -0700

    Login page

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -2,10 +2,12 @@ import { provideRouter, RouterConfig } from '@angular/router';
 
 import { AboutRoutes } from './+about/index';
 import { HomeRoutes } from './+home/index';
+import { LoginRoutes } from './+login/index';
 
 const routes: RouterConfig = [
   ...HomeRoutes,
-  ...AboutRoutes
+  ...AboutRoutes,
+  ...LoginRoutes
 ];
 
 export const APP_ROUTER_PROVIDERS = [
