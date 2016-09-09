b111885f00c2a585c3dd653e793c4e801495dd73
Ryan Wilson <Ryan@MacBook-Pro.local>
2016-09-08 19:34:12 -0700

    Countdown

--- src/client/app/+home/home.component.ts
+++ src/client/app/+home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 
+declare var Countdown: any;
+
 @Component({
   moduleId: module.id,
   selector: 'sd-home',
@@ -9,5 +11,17 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
   directives: [ROUTER_DIRECTIVES]
 })
 export class HomeComponent {
+
+  timeleft: string;
+
+  constructor() {
+    var target_date = new Date(2016, 10, 13, 19, 0, 0);
+    var current_date = new Date();
+    var count = new Countdown(target_date, current_date);
+    var self = this;
+    count.countdown(function (t: any) {
+      self.timeleft = `${t.days} days ${t.hours} hours ${t.minutes} minutes ${t.seconds} seconds`;
+    });
+  }
 
 }
--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -31,6 +31,7 @@ export class ProjectConfig extends SeedConfig {
     // Add `local` third-party libraries to be injected/bundled.
     this.APP_ASSETS = [
       ...this.APP_ASSETS,
+      {src: `${this.ASSETS_SRC}/js/countdown.min.js`, inject: true, vendor: false}
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
       // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
     ];
